baecd2a4caba6edbf009cd4de175820dee77b7ec
Omar <omar.gzz.rdz@hotmail.com>
2025-09-02 18:52:44 -0600

    change: vercel to cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import vercel from "@astrojs/vercel";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: cloudflare(),
 });
